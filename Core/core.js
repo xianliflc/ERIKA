35fc83f3fd1bc7e5abffc5eabef96ee6e9b246e8
xiali <xian.li@priceline.com>
2017-07-31 16:13:46 -0500

    initial commit and gitignor updates

---
+++ Core/core.js
@@ -0,0 +1,16 @@
+var core = (function(erika){
+    if( erika === undefined){
+        if( window.Erika !== undefined){
+            erika = window.Erika();
+        }
+        else{
+            console.log('Erika is not defined');
+            return;
+        }
+    }
+
+    return {
+//        logger:
+    };
+
+});
