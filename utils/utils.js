55de7179ddc3155fd49bac3dd525a339903c6442
xiali <xian.li@priceline.com>
2017-08-02 12:21:58 -0500

    add more util functions and structured better in utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -2,13 +2,33 @@ var utils = (function(erika){
 
     'use strict';
 
-    function isArray(o) {
-      return Object.prototype.toString.call(o) === '[object Array]';
+    var libs = {
+
+        'isArray' :     function isArray(o) {
+            return Object.prototype.toString.call(o) === '[object Array]' ;
+        },
+
+        'startsWith' :  function startsWith(body,str) {
+            return
+            body.indexOf(str) == 0;
+        }
+    };
+
+
+    var utils = {
+            'isArray' : libs.isArray,
+            'startsWith' :  libs.startsWith
+    };
+
+    if (typeof erika !== undefined){
+        if (typeof erika.utils !== undefined){
+            // todo some extra logic
+        }
     }
 
+    erika.utils = utils;
 
-    return {
-        issArray: isArray,
-    };
+    // if you want a return;
+    return erika;
 
 });
