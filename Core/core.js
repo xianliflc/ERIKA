2227c472f8f83dc7d635792464ffc28256000775
Xian Li <xian.li@Xians-MacBook-Pro.local>
2018-04-10 22:48:56 -0400

    add basic ajax function

--- Core/core.js
+++ Core/core.js
@@ -1,21 +1,82 @@
-var core = (function (erika) {
-
+window.Erika.core = (function () {
+    var dd = 1;
     'use strict';
+    function ajax(method, url, data, headers, cb, option) {
+        var xmlhttp = new XMLHttpRequest();
+        url = url || '';
 
-    function checkErikaExists() {
-        if (erika === undefined) {
-            if (window.Erika !== undefined) {
-                erika = window.Erika();
+        method = method || 'GET';
+        xmlhttp.onreadystatechange = function(eve) {
+            if (this.readyState == 4 && this.status == 200) {
+                var data = JSON.parse(this.responseText);
+                if (typeof cb === 'function') {
+                    cb(data,this);
+                }
+            } else if (this.readyState == 4 && this.status !== 200) {
+                if (typeof cb === 'function') {
+                    cb(null, this);
+                }
+            }
+
+        };
+
+        if (method.toUpperCase() === 'GET') {
+
+            url = url + '?' + buildQueryString(data);
+           
+            xmlhttp.open("GET", url, true);
+            sendHeaders(xmlhttp, headers);
+            xmlhttp.send();
+        } else if (method.toUpperCase() === 'POST') {
+            xmlhttp.open("POST", url, true);
+            sendHeaders(xmlhttp, headers);
+            if (option === 'json') {
+                xmlhttp.setRequestHeader('Accept', 'application/json');
+                xmlhttp.send(JSON.stringify(data));
             } else {
-                console.log('Erika is not defined');
-                return false;
+                xmlhttp.setRequestHeader('Accept', 'application/x-www-form-urlencoded');
+                xmlhttp.send(buildQueryString(data));
             }
         }
+
+    }
+
+    function sendHeaders(xmlhttp, headers) {
+        Object.keys(headers).forEach(function(element) {
+            xmlhttp.setRequestHeader(element, headers[element]);
+        });
+    }
+
+    function buildQueryString(params) {
+        var esc = encodeURIComponent;
+        var query = Object.keys(params)
+                    .map(k => esc(k) + '=' + esc(params[k]))
+                    .join('&');
+        return query;
+    }
+
+    /**
+     * 
+     * @param {*} url 
+     * @param {*} data 
+     * @param {*} header 
+     * @param {*} cb 
+     */
+    function get(url, data, header, cb) {
+        ajax('GET', url, data, header, cb);
+    }
+
+    function post(url, data, header, cb, option) {
+        ajax('POST', url, data, header, cb, option);
     }
 
 
     return {
-        exists: checkErikaExists,
+        ajax: ajax,
+        get: get,
+        post: post,
+        query: buildQueryString
     };
 
-});
+})();
+
