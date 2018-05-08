2f87e005e60a2ab0d5078e1c683e183f63f9679f
Xian Li <xian.li@Xians-MacBook-Pro.local>
2018-05-08 13:33:52 -0400

    minor updates on component structure

--- Core/component.js
+++ Core/component.js
@@ -5,17 +5,28 @@ Erika.Object = (function(){
     class Component {
         constructor (name, properties, context) {
             this.name = name;
+            this.context = context;
             for (const key in properties) {
                 if (properties.hasOwnProperty(key)) {
                      this[key] = properties[key];
                 }
             }
         }
+
+        name() {
+            return this.name;
+        }
+
+        use(extra_context) {
+            this.context = Object.assign(this.context, extra_context);
+            return this;
+        }
     }
 
     var create = function(name, properties, context) {
         return new Component(name, properties, context);
     };
+    
 
     return {
         'create' : create,
